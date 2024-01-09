71056095347a77416fafa2b7ac0054e83cc4fe7a
Tharun <tharunsenthilkumar308@gmail.com>
2024-01-09 07:43:35 -0600

    reformatting testing needed

--- src/main/java/team/gif/robot/Robot.java
+++ src/main/java/team/gif/robot/Robot.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package team.gif.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -56,8 +56,6 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
---
+++ src/main/java/team/gif/robot/UiSmartDashboard.java
@@ -0,0 +1,11 @@
+package team.gif.robot;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+public class UiSmartDashboard {
+
+    public UiSmartDashboard() {
+        ShuffleboardTab shuffleboardTab = Shuffleboard.getTab("SmartDashboard");
+    }
+}
