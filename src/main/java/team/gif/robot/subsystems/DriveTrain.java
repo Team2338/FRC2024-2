aca69119463720f41b7245283283ae0b3fc2765c
bobsfriend12 <calebsethherring+github@gmail.com>
2024-02-10 11:03:51 -0600

    get rid of MotorControllerGroup

--- src/main/java/team/gif/robot/subsystems/DriveTrain.java
+++ src/main/java/team/gif/robot/subsystems/DriveTrain.java
@@ -6,7 +6,6 @@ package team.gif.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team.gif.robot.RobotMap;
 
@@ -15,21 +14,22 @@ public class DriveTrain extends SubsystemBase {
     private WPI_TalonSRX rearLeftCIM;
     private WPI_TalonSRX frontRightCIM;
     private WPI_TalonSRX rearRightCIM;
-    MotorControllerGroup leftMotors;
-    MotorControllerGroup rightMotors;
     private DifferentialDrive drive;
     public DriveTrain() {
         frontLeftCIM = new WPI_TalonSRX(RobotMap.FRONT_LEFT_CIM);
         rearLeftCIM = new WPI_TalonSRX(RobotMap.REAR_LEFT_CIM);
         frontRightCIM = new WPI_TalonSRX(RobotMap.FRONT_RIGHT_CIM);
         rearRightCIM = new WPI_TalonSRX(RobotMap.REAR_RIGHT_CIM);
+
         frontLeftCIM.configFactoryDefault();
         rearLeftCIM.configFactoryDefault();
         frontRightCIM.configFactoryDefault();
         rearRightCIM.configFactoryDefault();
-        leftMotors = new MotorControllerGroup(frontLeftCIM, rearLeftCIM);
-        rightMotors = new MotorControllerGroup(frontRightCIM, rearRightCIM);
-        drive = new DifferentialDrive(leftMotors, rightMotors);
+
+        rearLeftCIM.follow(frontLeftCIM);
+        rearRightCIM.follow(frontRightCIM);
+
+        drive = new DifferentialDrive(frontLeftCIM, frontRightCIM);
     }
     public void driveArcade(double speed, double rotation) {drive.arcadeDrive(speed, rotation);}
     public void driveTank(double leftSpeed, double rightSpeed){drive.tankDrive(leftSpeed, rightSpeed);}
